771b90a990cc2cca7eb4032a627a79602a59706b
431948 <431948@SAIT222396IT.ACDM.DS.SAIT.CA>
2020-10-06 13:17:09 -0600

    Lab 4 Java Bean (User) created

--- src/java/servlets/AccountService.java
+++ src/java/servlets/AccountService.java
@@ -10,5 +10,12 @@ package servlets;
  * @author 431948
  */
 public class AccountService {
-    
+
+       public User login(String username, String password) {
+           if ((username.equals("adam") || username.equals("betty")) && password.equals("password"))
+               
+               return new User(username, null);
+           
+           return null;
+       }
 }
